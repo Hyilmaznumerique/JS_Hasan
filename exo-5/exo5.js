f9a8bf67338c31c75cb84d96cd1e4e192990de12
Hyilmaznumerique <hasan.yilmaz@le-campus-numerique.fr>
2025-01-07 14:45:43 +0100

    Exercice 5

--- exo-5/exo5.js
+++ exo-5/exo5.js
@@ -1,3 +1,36 @@
 // you can write js here
 
 console.log('exo-5');
+
+let input = 'Turpentine and turtles';
+
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+
+let resultArray = [];
+
+for (let i = 0; i < input.length; i++) {
+    console.log(input[i]);
+  }
+  
+  for (let i = 0; i < input.length; i++) {
+    if (vowels.indexOf(input[i].toLowerCase()) !== -1) {
+      resultArray.push(input[i]);
+    }
+  }
+
+  for (let i = 0; i < input.length; i++) {
+    for (let j = 0; j < vowels.length; j++) {
+      if (input[i].toLowerCase() === vowels[j]) {
+        resultArray.push(input[i]);
+      }
+    }
+  }
+  
+  for (let i = 0; i < input.length; i++) {
+    if (vowels.indexOf(input[i].toLowerCase()) !== -1) {
+      resultArray.push(input[i]);
+    }
+  }
+  
+  let whaleLanguage = resultArray.join('').toUpperCase();
+console.log(whaleLanguage);
